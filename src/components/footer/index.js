ea80a6b3b137cf01fd2d6ba975af5683299cfbe7
Uzairkhan55465 <uzairkhan55465@gmail.com>
2024-08-02 14:53:40 -0700

    aaa

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,8 +8,8 @@ const Footer = () => {
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src={Logo} class="h-8" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <img src={Logo} className='w-[180px] h-[80px]' alt="Flowbite Logo" />
+                 
                     </a>
                     <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                     <a href="#home" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 font-[500] mr-4">Home</a>
